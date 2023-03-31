343db692768a0bb8b8b1b24df522b7c595034450
navjack <jack.mangano@gmail.com>
2023-03-31 17:17:28 -0400

    Update command.py

--- command.py
+++ command.py
@@ -1,8 +1,5 @@
 import os
 
-def clear_screen():
-    os.system("cls" if os.name == "nt" else "clear")
-
 def execute_command(command):
     if command.lower() == "dir":
         os.system("ls")
@@ -10,11 +7,10 @@ def execute_command(command):
         os.system(command)
 
 def main():
+    print("Microsoft Windows 98 [Version 4.10.1998]")
+    print("(c)Copyright Microsoft Corp 1981-1999.")
+    print()
     while True:
-        clear_screen()
-        print("Microsoft Windows 98 [Version 4.10.1998]")
-        print("(c)Copyright Microsoft Corp 1981-1999.")
-        print()
         command = input("C:\\>")
         if command.lower() == "exit":
             break
