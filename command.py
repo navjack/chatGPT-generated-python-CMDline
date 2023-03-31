d7fe4578503c1d118b8932a4f460a991d9889b52
navjack <jack.mangano@gmail.com>
2023-03-31 17:18:05 -0400

    Update command.py

--- command.py
+++ command.py
@@ -1,8 +1,32 @@
 import os
+from datetime import datetime
+
+def format_file_size(size):
+    if size < 1024:
+        return f"{size} bytes"
+    elif size < 1024 * 1024:
+        return f"{size / 1024:.1f} KB"
+    else:
+        return f"{size / (1024 * 1024):.1f} MB"
+
+def format_dir_entry(entry):
+    if entry.is_file():
+        size = format_file_size(entry.stat().st_size)
+        mtime = datetime.fromtimestamp(entry.stat().st_mtime).strftime(
+            "%m/%d/%Y %I:%M %p"
+        )
+        return f"{entry.name:<25} {size:>10} {mtime}"
+    else:
+        return f"{entry.name:<25}    <DIR>"
+
+def execute_dir():
+    entries = sorted(os.scandir(), key=lambda e: (e.is_file(), e.name))
+    for entry in entries:
+        print(format_dir_entry(entry))
 
 def execute_command(command):
     if command.lower() == "dir":
-        os.system("ls")
+        execute_dir()
     else:
         os.system(command)
 
